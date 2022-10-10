e39758348d6161fafaa090578a870f8baa409943
yri066 <yri066.ru@gmail.com>
2022-10-10 23:01:36 +0300

    Добавил поддержку для профиля пользователя

--- Wallpaper/PublicationWallPaper.cs
+++ Wallpaper/PublicationWallPaper.cs
@@ -3,106 +3,101 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Wallpaper.Model;
 
 namespace Wallpaper
 {
-    internal class PublicationWallPaper
-    {
-        public IConfiguration AppConfiguration { get; set; }
-        private string VK_GROUP_ID { get; }
-        private string VK_GROUP_API_KEY { get; }
-        private string VkUrl { get; }
-        private string ArgumentsStartApp { get; }
+	internal class PublicationWallPaper
+	{
+		public IConfiguration AppConfiguration { get; set; }
+		private string VK_ID { get; }
+		private string VK_ACCESS_TOKEN { get; }
+		private string VkUrl { get; }
+		private string ArgumentsStartApp { get; }
 
-        public PublicationWallPaper(IConfiguration AppConfiguration)
-        {
-            this.AppConfiguration = AppConfiguration;
-            VK_GROUP_ID = AppConfiguration["VK_GROUP_ID"];
-            VK_GROUP_API_KEY = AppConfiguration["VK_GROUP_API_KEY"];
+		public PublicationWallPaper(IConfiguration AppConfiguration)
+		{
+			this.AppConfiguration = AppConfiguration;
+			VK_ID = AppConfiguration["VK_ID"];
+			VK_ACCESS_TOKEN = AppConfiguration["VK_ACCESS_TOKEN"];
 
-            string width = AppConfiguration["width"].ToString();
-            string height = AppConfiguration["height"].ToString();
-            VkUrl = "https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id=" + VK_GROUP_ID + "&crop_x=00&crop_y=0&crop_x2=" + width + "&crop_y2=" + height + "&&access_token=" + VK_GROUP_API_KEY + "&v=5.124";
+			string width = AppConfiguration["width"].ToString();
+			string height = AppConfiguration["height"].ToString();
 
-            ArgumentsStartApp = AppConfiguration["WEB_PAGE_URL"] + " " + width + " " + height;
-        }
-        public async Task SetImage()
-        {
-            if (VK_GROUP_ID == "" || VK_GROUP_API_KEY == "" || AppConfiguration["WEB_PAGE_URL"] == "")
-                return;
+			if (AppConfiguration["type"] == "group")
+			{
+				VkUrl = $"https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id={VK_ID}&crop_x=00&crop_y=0&crop_x2={width}&crop_y2={height}&&access_token={VK_ACCESS_TOKEN}&v=5.131";
+			}
+			else
+			{
+				VkUrl = $"https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?client_id={VK_ID}&crop_x=&crop_y=&crop_height={height}&crop_width={width}&access_token={VK_ACCESS_TOKEN}&v=5.191";
+			}
 
-            string output = RunProgram();
+			ArgumentsStartApp = $"{Path.Combine(Environment.CurrentDirectory, "HtmlRender", "HtmlToImage.dll")} {AppConfiguration["WEB_PAGE_URL"]} {width} {height}";
+		}
 
-            var bytes = new MemoryStream(Convert.FromBase64String(output)).ToArray();
+		public async Task SetImage()
+		{
+			if (VK_ID == "" || VK_ACCESS_TOKEN == "" || AppConfiguration["WEB_PAGE_URL"] == "")
+				return;
 
-            string SendUrlJson = await PostToUrl(VkUrl);
-            string SendUrl = JsonSerializer.Deserialize<PostUrl>(SendUrlJson).response.upload_url;
+			string output = RunProgram();
 
-            string SendPhotoJson = PostImage(SendUrl, bytes);
-            var SendPhoto = JsonSerializer.Deserialize<SetPhoto>(SendPhotoJson);
+			var bytes = new MemoryStream(Convert.FromBase64String(output)).ToArray();
 
-            string hash = SendPhoto.hash;
-            string photo = SendPhoto.photo;
+			string SendUrlJson = await PostToUrl(VkUrl);
 
-            string SendPhotoUrl = "https://api.vk.com/method/photos.saveOwnerCoverPhoto?hash=" + hash + "&photo=" + photo + "&access_token=" + VK_GROUP_API_KEY + "&v=5.124";
-            await PostToUrl(SendPhotoUrl);
-        }
+			string SendUrl = JsonSerializer.Deserialize<PostUrl>(SendUrlJson).response.upload_url;
 
+			string SendPhotoJson = PostImage(SendUrl, bytes);
+			var SendPhoto = JsonSerializer.Deserialize<SetPhoto>(SendPhotoJson);
 
-        string PostImage(string url, byte[] imgByte)//загрузить изображение по ссылке
-        {
-            HttpClient httpClient = new HttpClient();
-            MultipartFormDataContent form = new MultipartFormDataContent();
+			string hash = SendPhoto.hash;
+			string photo = SendPhoto.photo;
 
-            byte[] imagebytearraystring = imgByte;
-            form.Add(new ByteArrayContent(imagebytearraystring, 0, imagebytearraystring.Count()), "photo", "image.png");
-            HttpResponseMessage response = httpClient.PostAsync(url, form).Result;
-
-            httpClient.Dispose();
-            string rez = response.Content.ReadAsStringAsync().Result;
-            return rez;
-        }
+			string SendPhotoUrl = $"https://api.vk.com/method/photos.saveOwnerCoverPhoto?hash={hash}&photo={photo}&access_token={VK_ACCESS_TOKEN}&v=5.131";
+			await PostToUrl(SendPhotoUrl);
+		}
 
 
-        async Task<string> PostToUrl(string url)//отправить запрос
-        {
-            string rez = null;
-            WebRequest request = WebRequest.Create(url);
-            WebResponse response2 = await request.GetResponseAsync();
-            using (Stream stream = response2.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    rez = reader.ReadToEnd();
-                }
-            }
-            response2.Close();
-            return rez;
-        }
+		string PostImage(string url, byte[] imgByte)//отправить изображение по ссылке
+		{
+			HttpClient httpClient = new HttpClient();
+			MultipartFormDataContent form = new MultipartFormDataContent();
 
-        string RunProgram()
-        {
-            Process p = new Process();
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
+			byte[] imagebytearraystring = imgByte;
+			form.Add(new ByteArrayContent(imagebytearraystring, 0, imagebytearraystring.Count()), "photo", "image.png");
+			HttpResponseMessage response = httpClient.PostAsync(url, form).Result;
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                p.StartInfo.FileName = Environment.CurrentDirectory + @"\HtmlRender\HtmlToImage.exe";
+			httpClient.Dispose();
+			string rez = response.Content.ReadAsStringAsync().Result;
+			return rez;
+		}
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                p.StartInfo.FileName = Environment.CurrentDirectory + @"/HtmlRender/HtmlToImage";
 
-            p.StartInfo.Arguments = ArgumentsStartApp;
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            p.WaitForExit();
-            return output;
-        }
-    }
+		async Task<string> PostToUrl(string url)//отправить запрос
+		{
+			using var client = new HttpClient();
+
+			return await client.GetStringAsync(url);
+		}
+
+		string RunProgram()
+		{
+			Process p = new Process();
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.FileName = "dotnet";
+			p.StartInfo.Arguments = ArgumentsStartApp;
+			p.Start();
+
+			string output = p.StandardOutput.ReadToEnd();
+			p.WaitForExit();
+
+			return output;
+		}
+	}
 }
